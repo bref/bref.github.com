793d9947fcda07fbe9b3515c8519e5304049cf53
Vincent Giersch <vincent@giersch.fr>
2012-03-21 14:40:18 +0100

    Intro modules + CGI

--- examples/generate.sh
+++ examples/generate.sh
@@ -12,3 +12,7 @@ for doc in $todoc; do
     cat documentation-module.header.tpl.html $docdir.tmp documentation-module.footer.tpl.html > documentation-example-$docbase.html
     rm $docdir.tmp
 done
+
+sed -ie "1, /<div class='span6 example-doc-text'>/ {
+s@<div class='span6 example-doc-text'>@<div class='span6 example-doc-text'><iframe width='540' height='315' src='http://www.youtube.com/embed/rdKvE2qo70g' frameborder='0' allowfullscreen></iframe>@
+}" documentation-example-ModCGI.html
